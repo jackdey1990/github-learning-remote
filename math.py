f2e94e65cf44400819eeb1020e70dec7458e489c
jackdey1990 <98075132+jackdey1990@users.noreply.github.com>
2022-01-20 14:39:38 +0530

    Add files via upload
    added_file

---
+++ math.py
@@ -0,0 +1,13 @@
+#addition
+def add(x,y):
+    return(x+y)
+def sub(x,y):
+    return(x-y)
+def mul(x,y):
+    return(x*y)
+def div(x,y):
+    return(x/y)
+def mod(x,y):
+    return(x%y)
+def square(x):
+    return x*x
